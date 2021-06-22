cc15b74544e1bfe136022b6d3f18bb159013a522
MikeMerz <mikemerzl13@gmail.com>
2021-06-22 23:07:26 +0300

    first push

--- app/src/main/java/com/example/ex8/MainActivity.kt
+++ app/src/main/java/com/example/ex8/MainActivity.kt
@@ -1,11 +1,23 @@
 package com.example.ex8
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
+    var holderImpl:CalcHolderImpl?=null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val app: CalcApp = getApplicationContext() as CalcApp
+        this.holderImpl = app.db
+        if (holderImpl == null) {
+            holderImpl = CalcHolderImpl(this)
+        }
+        val adapter = CalcAdapterImpl(holderImpl!!)
+        val calcRecyler :RecyclerView = findViewById(R.id.recyler)
+        calcRecyler.adapter =adapter
+        calcRecyler.layoutManager = LinearLayoutManager(this,RecyclerView.VERTICAL,false)
     }
 }
